c78802e1fa0fbc6ce0ace3383a45fd1d56db51ef
chhamzarashid <ch.hamzarashid@outlook.com>
2020-12-03 01:34:55 +0500

    Add Functions

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from matplotlib import pyplot as plt
 from cv2 import cv2
 
 until.canny("1.jpg")
+until.laplacian("1.jpg")
+until.sobel("1.jpg")
 
 
 
--- until.py
+++ until.py
@@ -6,9 +6,31 @@ def canny(path):
    
     cv2.waitKey(0)
     cv2.destroyAllWindows
-    plt.subplot(121),plt.imshow(orig_im,cmap = 'gray')
+    plt.subplot(1,2,1),plt.imshow(orig_im,cmap = 'gray')
     plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(cannyimg,cmap = 'gray')
+    plt.subplot(1,2,2),plt.imshow(cannyimg,cmap = 'gray')
     plt.title('Canny Image'), plt.xticks([]), plt.yticks([])
     plt.show()
-
+def laplacian(path1):
+    orig_im = cv2.imread(path1,0)
+    laplacianimg =  cv2.Laplacian(orig_im ,cv2.CV_64F)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows
+    plt.subplot(1,2,1),plt.imshow(orig_im,cmap = 'gray')
+    plt.title('Original'), plt.xticks([]), plt.yticks([])
+    plt.subplot(1,2,2),plt.imshow(laplacianimg,cmap = 'gray')
+    plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+    plt.show()
+def sobel(path):
+     orig_im = cv2.imread(path,0)
+     sobelx = cv2.Sobel(orig_im,cv2.CV_64F,1,0, ksize=5) 
+     sobely = cv2.Sobel(orig_im,cv2.CV_64F,0,1,ksize=5)  
+     cv2.waitKey(0)
+     cv2.destroyAllWindows
+     plt.subplot(2,2,1),plt.imshow(orig_im,cmap = 'gray')
+     plt.title('Original'), plt.xticks([]), plt.yticks([])
+     plt.subplot(2,2,2),plt.imshow(sobelx,cmap = 'gray')
+     plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+     plt.subplot(2,2,3),plt.imshow(sobely,cmap = 'gray')
+     plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+     plt.show()
